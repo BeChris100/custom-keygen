083cfd07e2ef57cdb072a38b63264220588b5af0
BeChris100 <bechris.100@outlook.com>
2024-02-17 16:52:59 +0100

    Code Update

--- src/KeygenMain.java
+++ src/KeygenMain.java
@@ -1,2 +1,194 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class KeygenMain {
+
+    private static final int[] divisors = {27, 16, 21},
+            lengths = {9, 4, 7};
+
+    private static final Random rnd = new Random();
+    private static final Scanner sc = new Scanner(System.in);
+
+    private static String mkHexNumber(int length, int divisor) {
+        boolean valid = false;
+        String number = "";
+
+        while (!valid) {
+            StringBuilder ss = new StringBuilder();
+            boolean hasDigit = false, hasAlpha = false;
+
+            for (int i = 0; i < length; i++) {
+                int digit = rnd.nextInt(16);
+                if (digit < 10) hasDigit = true;
+                else hasAlpha = true;
+
+                ss.append(Integer.toHexString(digit));
+            }
+
+            number = ss.toString();
+            long l = Long.parseLong(number, 16);
+            if (hasDigit && hasAlpha && l % divisor == 0)
+                valid = true;
+        }
+
+        return number;
+    }
+
+    private static String mkKey() {
+        String[] parts = new String[3];
+        for (int i = 0; i < divisors.length; i++)
+            parts[i] = mkHexNumber(lengths[i], divisors[i]);
+
+        return String.format("1983-%s-%s-%s", parts[0], parts[1], parts[2]);
+    }
+
+    private static long keys = 10; // Number of keys to generate for each thread
+    private static File outFile = null;
+    private static FileOutputStream fos;
+    private static final List<Thread> completedThreads = new ArrayList<>();
+
+    private static void ktg(long threads) {
+        long keysToGen = keys * threads;
+        if (keysToGen > Integer.MAX_VALUE) {
+            System.err.println("Cannot go beyond the Integer maximum value");
+            System.err.printf("Given %d, maximum Integer value is %d\n", keysToGen, Integer.MAX_VALUE);
+            System.err.println("Cannot process values");
+            System.exit(1);
+        } else if (keysToGen > 10_000) {
+            System.err.println("Generating keys above 10,000 might overload the system, causing performance to drop.");
+            System.err.println("You have currently set to " + keysToGen);
+            System.err.println("If you wish to continue with this selection, enter \"Continue\" (case irrelevant), or type in a smaller amount of keys per thread.");
+            System.err.println("Entering \"Exit\" will close this App.");
+            System.err.println();
+            System.err.print("Your selection: ");
+
+            String choice = sc.nextLine().trim();
+            if (choice.isEmpty())
+                return;
+
+            if (choice.contains(" "))
+                choice = choice.replaceAll(" ", "");
+
+            if (choice.equalsIgnoreCase("Exit") || choice.equalsIgnoreCase("Quit"))
+                System.exit(0);
+
+            if (choice.equalsIgnoreCase("Continue"))
+                return;
+
+            if (choice.contains(","))
+                choice = choice.replaceAll(",", "");
+
+            if (choice.contains("."))
+                choice = choice.replaceAll("\\.", "");
+
+            if (choice.contains("_"))
+                choice = choice.replaceAll("_", "");
+
+            try {
+                long ignore = Long.parseLong(choice);
+                ktg(threads);
+            } catch (NumberFormatException ignore) {
+                System.err.println("Invalid number!");
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        long threads = 1; // One thread for * keys, where '*' is the amount of keys
+
+        if (args != null && args.length >= 1) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+
+                if (arg.startsWith("-k")) {
+                    if (arg.contains("."))
+                        arg = arg.replaceAll("\\.", "");
+
+                    if (arg.contains(","))
+                        arg = arg.replaceAll(",", "");
+
+                    if (arg.contains("_"))
+                        arg = arg.replaceAll("_", "");
+
+                    try {
+                        keys = Long.parseLong(arg.substring(2));
+                    } catch (NumberFormatException ignore) {
+                        System.err.println("In argument " + (i + 1) + ", received " + arg + "; should receive -k10 with a valid number");
+                        System.exit(1);
+                    }
+                }
+
+                if (arg.startsWith("-t")) {
+                    if (arg.contains("."))
+                        arg = arg.replaceAll("\\.", "");
+
+                    if (arg.contains(","))
+                        arg = arg.replaceAll(",", "");
+
+                    if (arg.contains("_"))
+                        arg = arg.replaceAll("_", "");
+
+                    try {
+                        threads = Long.parseLong(arg.substring(2));
+                    } catch (NumberFormatException ignore) {
+                        System.err.println("In argument " + (i + 1) + ", received " + arg + "; should receive -t1 with a valid number");
+                        System.exit(1);
+                    }
+                }
+
+                if (arg.startsWith("-o"))
+                    outFile = new File(arg.substring(2));
+            }
+        }
+
+        if (threads > 20000) {
+            System.err.println("Overloading the Thread count might lead to the program crashing.");
+            System.err.println("Setting the Thread count down to 100");
+
+            threads = 100;
+        }
+
+        ktg(threads);
+
+        for (int thIndex = 0; thIndex < threads; thIndex++) {
+            Thread th = new Thread(() -> {
+                for (int k = 0; k < keys; k++) {
+                    String key = mkKey();
+                    System.out.println(key);
+
+                    try {
+                        if (outFile != null) {
+                            if (fos == null)
+                                fos = new FileOutputStream(outFile);
+
+                            fos.write(key.getBytes(StandardCharsets.UTF_8));
+                            fos.write("\n".getBytes(StandardCharsets.UTF_8));
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace(System.err);
+                    }
+                }
+
+                completedThreads.add(Thread.currentThread());
+            });
+            th.setName("KeyGen Thread " + (thIndex + 1));
+            th.start();
+        }
+
+        if (completedThreads.size() == threads) {
+            try {
+                fos.close();
+            } catch (IOException ex) {
+                ex.printStackTrace(System.err);
+            }
+        }
+    }
+
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,3 +1,10 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -6,61 +13,181 @@ public class MainClass {
     // Key format: 1983-XXXXXXXXX-XXXX-XXXXXXX
 
     private static final Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[A-Fa-f])[0-9A-Fa-f]+$");
+    private static final Scanner sc = new Scanner(System.in);
+    private static final List<String> keysToProcess = new ArrayList<>();
+
+    private static File outFile;
 
     private static boolean isCodeValid(String str) {
         return pattern.matcher(str).matches();
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your key: ");
-        String key = sc.nextLine();
+    private static boolean isKeyInvalid(String entry, int index, String key, boolean beginning) {
+        if (beginning) {
+            if (!key.startsWith("1983"))
+                return true;
 
-        if (!key.startsWith("1983")) {
-            System.err.println("Invalid key!");
-            System.exit(1);
-        }
-
-        String[] spl = key.split("-", 4);
-        if (spl.length != 4) {
-            System.err.println("Invalid key!");
-            System.exit(1);
-        }
-
-        for (int i = 1; i < spl.length; i++) {
+            String[] _spl = key.split("-", 4);
+            return _spl.length != 4;
+        } else {
             try {
-                String entry = spl[i];
+                if (!key.startsWith("1983"))
+                    return true;
 
-                if (!isCodeValid(entry)) {
-                    System.err.println("Invalid key!");
-                    System.exit(1);
+                if (!isCodeValid(entry))
+                    return true;
+
+                long l;
+                if (index == 0)
+                    l = Long.parseLong(entry);
+                else
+                    l = Long.parseLong(entry, 16);
+
+                return switch (index) {
+                    case 0 -> l != 1983;
+                    case 1 -> l % 27 != 0;
+                    case 2 -> l % 16 != 0;
+                    case 3 -> l % 21 != 0;
+                    default -> throw new IndexOutOfBoundsException("Index got thrown out of bounds; received " + index);
+                };
+            } catch (NumberFormatException ignore) {
+                return true;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args != null && args.length >= 1) {
+            for (int i = 0; i < args.length; i++) {
+                String arg = args[i];
+
+                if (arg.startsWith("-i")) {
+                    if (!keysToProcess.isEmpty())
+                        keysToProcess.clear();
+
+                    try {
+                        FileInputStream fis = new FileInputStream(arg.substring(2));
+                        byte[] buff = new byte[1024];
+                        int len;
+                        StringBuilder str = new StringBuilder();
+
+                        while ((len = fis.read(buff, 0, 1024)) != -1)
+                            str.append(new String(buff, 0, len));
+
+                        fis.close();
+
+                        List<String> keysList = new ArrayList<>();
+                        for (String key : str.toString().split("\n")) {
+                            if (key.trim().isEmpty())
+                                continue;
+
+                            if (keysList.contains(key))
+                                continue;
+
+                            keysList.add(key);
+                        }
+
+                        keysToProcess.addAll(keysList);
+                    } catch (IOException ex) {
+                        System.err.println("While opening " + arg.substring(2) + " (arg = " + (i + 1) + "), an error occurred.");
+                        ex.printStackTrace(System.err);
+                        System.exit(1);
+                    }
                 }
 
-                long l = Long.parseLong(entry, 16);
-                entry = String.valueOf(l);
-
-                if (i == 1 && entry.length() != 9 && l % 27 != 0) {
-                    System.err.println("Invalid key!");
-                    System.exit(1);
-                }
-
-                if (i == 2 && entry.length() != 4 && l % 16 != 0) {
-                    System.err.println("Invalid key!");
-                    System.exit(1);
-                }
-
-                if (i == 3 && entry.length() != 7 && l % 21 != 0) {
-                    System.err.println("Invalid key!");
-                    System.exit(1);
-                }
-            } catch (NumberFormatException ex) {
-                System.err.println("Invalid key!");
-                ex.printStackTrace();
-                System.exit(1);
+                if (arg.startsWith("-o"))
+                    outFile = new File(arg.substring(2));
             }
         }
 
-        System.out.println("Key valid! Given key: " + key);
+        if (keysToProcess.isEmpty()) {
+            System.out.print("Enter your key: ");
+            String key = sc.nextLine();
+
+            if (isKeyInvalid(null, 0, key, true)) {
+                System.err.println("Invalid key!");
+                System.exit(1);
+            }
+
+            String[] spl = key.split("-");
+            if (spl.length != 4) {
+                System.err.println("Invalid key!");
+                System.exit(1);
+            }
+
+            for (int i = 0; i < spl.length; i++) {
+                if (i == 0)
+                    continue;
+
+                if (isKeyInvalid(spl[i], i, key, false)) {
+                    System.err.println("Invalid key!");
+                    System.exit(1);
+                }
+            }
+
+            System.out.println("Key valid!");
+            return;
+        }
+
+        List<KeyProcess> keyProcessList = new ArrayList<>();
+        for (String key : keysToProcess) {
+            if (key.trim().isEmpty())
+                continue;
+
+            String[] splits = key.split("-");
+            if (splits.length != 4) {
+                keyProcessList.add(new KeyProcess(key, false));
+                continue;
+            }
+
+            try {
+                long e0 = Long.parseLong(splits[0]),
+                        e1 = Long.parseLong(splits[1], 16),
+                        e2 = Long.parseLong(splits[2], 16),
+                        e3 = Long.parseLong(splits[3], 16);
+
+                keyProcessList.add(new KeyProcess(key,
+                        e0 == 1983 &&
+                        e1 % 27 == 0 &&
+                        e2 % 16 == 0 &&
+                        e3 % 21 == 0));
+            } catch (NumberFormatException ignore) {
+            }
+        }
+
+        int validProcesses = 0, invalidProcesses = 0;
+
+        for (KeyProcess process : keyProcessList) {
+            if (process.valid())
+                validProcesses++;
+            else
+                invalidProcesses++;
+        }
+
+        System.out.println("Processed keys: " + (validProcesses + invalidProcesses));
+        System.out.println("Valid keys: " + validProcesses);
+        System.out.println("Invalid keys: " + invalidProcesses);
+
+        if (outFile != null) {
+            try {
+                FileOutputStream fos = new FileOutputStream(outFile);
+
+                for (KeyProcess process : keyProcessList) {
+                    if (process.valid()) {
+                        fos.write(process.key().getBytes(StandardCharsets.UTF_8));
+                        fos.write("\n".getBytes(StandardCharsets.UTF_8));
+                    }
+                }
+
+                fos.close();
+            } catch (IOException ex) {
+                System.err.println("Could not open the output file");
+                ex.printStackTrace(System.err);
+            }
+        }
+    }
+
+    record KeyProcess(String key, boolean valid) {
     }
 
 }
